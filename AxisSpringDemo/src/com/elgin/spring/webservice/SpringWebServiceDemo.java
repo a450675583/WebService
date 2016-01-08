b188d10e03e1c5b29b1a43c4fd9438d37641ead0
elgin <450675583@qq.com>
2016-01-08 16:22:02 +0800

    initial

---
+++ AxisSpringDemo/src/com/elgin/spring/webservice/SpringWebServiceDemo.java
@@ -0,0 +1,21 @@
+package com.elgin.spring.webservice;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component("springWebService")
+public class SpringWebServiceDemo {
+    
+	public String springHello(){
+		return "hello spring-axis2"; 
+	}
+	
+	public int getAge(){
+		return new Random().nextInt(80);
+	}
+	
+	public void update(){
+		System.out.println("update something..");
+	}
+}
